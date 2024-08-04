c849bea81a8f650c0d997ba35f01c155870044d3
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-04 09:50:04 +0700

    finish Chuong_3_Bai_3.1_BTVN

---
+++ chuong_3/Bai_3_BTVN.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <limits.h> 
+#include <stdbool.h> 
+
+#define MAX_SIZE 100
+
+void XuatCotSoLe(int a[][MAX_SIZE], int rows, int cols) {
+    printf("Cac cot chi chua so le:\n");
+    for (int j = 0; j < cols; j++) {
+        bool allOdd = true;
+        for (int i = 0; i < rows; i++) {
+            if (a[i][j] % 2 == 0) {
+                allOdd = false;
+                break;
+            }
+        }
+        if (allOdd) {
+            printf("Cot %d: ", j);
+            for (int i = 0; i < rows; i++) {
+                printf("%d ", a[i][j]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main() {
+	int a[MAX_SIZE][MAX_SIZE];
+	int rows, cols;
+
+	printf("Nhap so dong cua ma tran: ");
+	scanf("%d", &rows);
+	printf("Nhap so cot cua ma tran: ");
+	scanf("%d", &cols);
+
+	printf("Nhap cac phan tu cua ma tran:\n");
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	XuatCotSoLe(a, rows, cols);
+	return 0;
+}
