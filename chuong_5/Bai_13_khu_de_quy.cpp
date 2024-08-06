358782b8f741ead5ea2f63f86b9324be253407d5
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-06 21:49:28 +0700

    finish Chuong_5_Bai_13_khu_de_quy_BTVN

---
+++ chuong_5/Bai_13_khu_de_quy.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+// Hàm tính phần tử thứ n của cấp số nhân (khử đệ quy)
+long long CapSoNhankhudequy(int n, long long a, long long q) {
+	long long result = a;
+	for (int i = 1; i < n; i++) {
+		result *= q;
+	}
+	return result;
+}
+
+int main() {
+	int n;
+	long long a, q;
+
+	printf("Nhap gia tri hang dau a: ");
+	scanf("%lld", &a);
+	printf("Nhap gia tri cong boi q: ");
+	scanf("%lld", &q);
+	printf("Nhap gia tri phan tu thu n: ");
+	scanf("%d", &n);
+
+	long long result = CapSoNhankhudequy(n, a, q);
+
+	printf("Phan tu thu %d cua cap so nhan la: %lld\n", n, result);
+
+	return 0;
+}
