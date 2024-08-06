4879ee01a20ceea35abc3037bf43109d76c5575e
dngtundt <dtun11111@gmail.com>
2024-08-06 22:11:26 +0700

    finished_chuong_6_BT_3_3_deQuy

---
+++ chuong_6/BT_3_3_deQuy.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void printOddFibonacciInRange(int m, int n, int current = 1, int next = 1) {
+    if (current > n) {
+        return;
+    }
+    if (current >= m && current % 2 != 0) {
+        printf("%d ", current);
+    }
+    printOddFibonacciInRange(m, n, next, current + next);
+}
+
+int main() {
+    int m = 10, n = 30;
+    printf("Các số Fibonacci lẻ trong đoạn [%d, %d]: ", m, n);
+    printOddFibonacciInRange(m, n);
+    printf("\n");
+    return 0;
+}
